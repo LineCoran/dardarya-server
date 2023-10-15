fe55f24af42631764aa28c781a1f8bda483f0bb7
alexey <dev.babimbo@gmail.com>
2023-10-15 16:07:50 +0300

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import orderRouter from './routes/order.js';
 import fileUpload from 'express-fileupload';
 import path from 'path'
 import { fileURLToPath } from 'url';
-// import fs from 'fs';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,16 +15,30 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, '.output/static')))
 app.use(cors());
 app.use(fileUpload({}));
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log('Server started on port: ', port)
-})
-
-// fs.mkdir('static', err => {
-//     if(err) throw err;
-// });
-
 app.use('/', orderRouter)
+
+const port = process.env.PORT || 3000
+
+async function start() {
+    try {
+      app.listen(port, async () => {
+        process.stdout.write(`Server is running. PORT: ${port} \n`);
+  
+        try {
+          await fs.access(path.resolve(__dirname, '..', 'public'), constants.F_OK);
+        } catch (e) {
+            process.stdout.write('folder PUBLIC does not exists. creating...');
+          await fs.mkdir(path.resolve(__dirname, 'public'), (err) => {
+            if (err) process.stdout.write('Error to create folder...');
+          });
+        }
+      });
+    } catch (e) {
+      process.stdout.write(`${e} \n`);
+    }
+  }
+
+  start();
 
 
 
