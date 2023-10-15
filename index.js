5a920c0976ff3757cff75d0a55a2816982c81542
alexey <dev.babimbo@gmail.com>
2023-10-15 15:05:40 +0300

    fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(cors());
 app.use(fileUpload({}));
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log('Server started on port: ', port)
 })
